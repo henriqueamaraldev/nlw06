74f9cd697fb2e5d1c7a8054d3e56aed5788fe628
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2021-10-20 17:46:48 -0300

    Changes
    Tentando solucionar o erro do throw catch

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,15 +3,15 @@ import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUserController {
 
-    async handle(req: Request, res: Response) {
+    async handle(request: Request, response: Response) {
 
-        const { name, email, admin } = req.body;
+        const { name, email, admin } = request.body;
 
         const createUserService = new CreateUserService();
 
         const user = await createUserService.execute({name, email, admin})
 
-        return res.json(user);
+        return response.json(user);
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import "./database";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(router);
 
