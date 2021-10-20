b935aeb504253caa403062a3bae79b393aa65c0e
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2021-10-20 16:36:03 -0300

    Second Commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import "reflect-metadata";
+import { router } from "./routes";
+import "./database";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+const port = 3000;
+app.listen(port, () => console.log('Server is running!'))
