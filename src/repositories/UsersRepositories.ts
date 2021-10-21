f8244654067c58e66775122fe8ddb5e431a8406c
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2021-10-21 10:55:38 -0300

    Solving Problems

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -2,6 +2,6 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from "../entities/User"
 
 @EntityRepository()
-class UsersRepositories extends Repository<User>{}
+class UsersRepositories extends Repository<User> {}
 
-export { UsersRepositories }
+export { UsersRepositories };
