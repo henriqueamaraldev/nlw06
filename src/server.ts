5508be51f2f1e260095981edd1e8d46eeff70931
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2021-10-22 15:40:39 -0300

    Tags
    Implementação do sistema de tags

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import { router } from "./routes";
 import "./database/index";
@@ -8,6 +9,19 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response:Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error."
+    })
+})
 
 const port = 3000;
 app.listen(port, () => console.log('Server is running!'))
