74bdf8d9f1151d6f81289d796c9f85bea07335da
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2021-10-27 17:48:36 -0300

    Incluindo relatorio de projeto

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import  { v4 as uuid } from "uuid";
-import { Expose } from 'class-transformer'
+
 
 @Entity('tags')
 class Tag {
